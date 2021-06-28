ae05013449208503180589d89b5d3a3f5bbe76e9
long148 <71367287+long148@users.noreply.github.com>
2021-06-28 18:10:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,41 @@
-<?php include_once("login.html"); ?>
+<?php include_once("login.html");
+
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "demo4";
+
+$conn = mysqli_connect($server, $user, $pass, $database);
+
+if (!$conn) {
+    die("<script>alert('Connect failed, vui lòng kết nối với database')</script>");
+}
+
+
+session_start();
+
+error_reporting();
+
+if (isset($_SESSION['username'])) {
+    header("Location: welcome.php");
+}
+
+if (isset($_POST['submit'])) {
+    $email = $_POST ['email'];
+    $password = md5($_POST['password']);
+
+
+    $sql = "SELECT *FROM users WHERE email='$email' AND password= '$password' ";
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        header("Location: welcome.php");
+    } else {
+        echo "<script>alert('Sai email hoặc password rồi')</script";
+    }
+}
+?>
+?>
 
